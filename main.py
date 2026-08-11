b2906725dd2212c59091076c9a3f0881eb2be7e2
shoh03 <sh.oh031128@gmail.com>
2026-08-11 21:06:20 +0900

    현재 시간 추가

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 import os, time, random
 st.title("Hello Streamlit")
 st.write('안녕하세요.')
-st.write(time.strftime('%Y-%m-%d %H:%M:%S')
+st.write(time.strftime('%Y-%m-%d %H:%M:%S'))
